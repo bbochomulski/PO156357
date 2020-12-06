05d7cf6e86f6b099d61ef77b601cd923702eb9ab
bbochomulski <b.bochomulski@gmail.com>
2020-12-06 17:38:27 +0100

    Lab10 Part2

--- Lab10/src/pl/imiajd/Bochomulski/TestOsoba.java
+++ Lab10/src/pl/imiajd/Bochomulski/TestOsoba.java
@@ -1,12 +1,20 @@
 package pl.imiajd.Bochomulski;
 
+import java.util.ArrayList;
+
 public class TestOsoba {
     public static void main(String[] args) {
 
+        ArrayList<Osoba> osoby = new ArrayList<Osoba>();
+        osoby.add(new Osoba("Kowalski", "1998-01-13"));
+        osoby.add(new Osoba("Nowak", "1980-01-03"));
+        osoby.add(new Osoba("Kowalski", "1970-03-01"));
+        osoby.add(new Osoba("Kaminski", "1980-01-03"));
+        osoby.add(new Osoba("Kowalczyk", "1989-10-20"));
+        System.out.println(osoby);
+        osoby.sort(Osoba::compareTo);
+        System.out.println(osoby);
 
-        Osoba o = new Osoba("Kowalski", "1998-01-13");
-
-        System.out.println(o.toString());
 
     }
 }
